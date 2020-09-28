79f8e71f09597796eb6b7dff9f031bd040fe6ea4
dxania <kxania@gmail.com>
2020-09-28 13:08:10 +0300

    Add all files

---
+++ APIs/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+from flask import Flask
+from flask_cors import CORS
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# CORS(app, allow_headers=[
+#     "Content-Type", "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers"],
+#     supports_credentials=True, intercept_exceptions=False)
+
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+# CORS(app, resources=r"/api/*")
+# CORS(app)
+# logging.getLogger('flask_cors').level = logging.DEBUG
+
+# app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+
+# jwt = JWTManager(app)
+
+from APIs.routes import app
